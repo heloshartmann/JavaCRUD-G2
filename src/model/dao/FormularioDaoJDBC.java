545d6789518e03e4d83e072b1fbecd12209f8bba
MestreDoPortal <joaovitorcand@outlook.com>
2023-11-01 14:35:30 -0300

    form jdbc

---
+++ src/model/dao/FormularioDaoJDBC.java
@@ -0,0 +1,49 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FormularioDAO {
+    private Connection connection; // A conexão JDBC com o banco de dados
+
+    // Construtor que recebe a conexão
+    public FormularioDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    // Método para inserir um novo Formulario no banco de dados
+    public void inserirFormulario(Formulario formulario) throws SQLException {
+        String sql = "INSERT INTO formulario (nome, conformidade, empresa_id, funcionario_id) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, formulario.getNome());
+            statement.setFloat(2, formulario.getConformidade());
+            statement.setInt(3, formulario.getEmpresa().getIDEmpresa());
+            statement.setInt(4, formulario.getFuncionario().getIDFuncionario());
+            statement.executeUpdate();
+        }
+    }
+
+    // Método para buscar um Formulario pelo ID
+    public Formulario buscarFormularioPorID(int IDforms) throws SQLException {
+        String sql = "SELECT * FROM formulario WHERE IDforms = ?";
+        Formulario formulario = null;
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, IDforms);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                formulario = new Formulario(
+                    resultSet.getInt("IDforms"),
+                    resultSet.getString("nome"),
+                    resultSet.getFloat("conformidade"),
+                    /* Recupere a Empresa e o Funcionario, se necessário */
+                );
+            }
+        }
+
+        return formulario;
+    }
+
+    // Outros métodos, como atualizar e excluir, podem ser adicionados de maneira semelhante.
+}
--- src/model/dao/impl/FormularioDao.java
+++ src/model/dao/impl/FormularioDao.java
@@ -1,18 +1,17 @@
 package model.dao;
 
-
+import java.util.List;
 
 import model.dao.entites.Formulario;
 
 
-public class FormularioDao {
+
     public interface FormularioDao {
 
-        void insert(FormularioDao obj);
-        void update(FormularioDao obj);
+        void insert(Formulario obj);
+        void update(Formulario obj);
         void deleteById(Integer id);
         Formulario findById(Integer id);
         List<Formulario> findAll();
-        List<Formulario> findByDepartment(Department department);
     }
-}
+
