e8a80898ff011648384579ac8a94a8c04c29feca
MestreDoPortal <joaovitorcand@outlook.com>
2023-11-28 16:34:21 -0300

    formulario interface

--- src/application/CadastrarFormularios.java
+++ src/application/CadastrarFormularios.java
@@ -1,11 +1,82 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.FuncionarioDao;
+import model.dao.entites.Funcionario;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class CadastrarFormularios extends JFrame {
-    private JPanel TelaCadastrarFormulario;
+    private JPanel TelaCadastrarFormularios;
+    private JTextField textFieldConformidade;
+    private JTextField textFieldCategoria;
+    private JTextField textFieldIdEmpresa;
+    private JTextField textFieldIdFuncionario;
+    private JButton CadastrarFormulariosButton;
 
-    public CadastrarFormularios (){
+    public CadastrarFormularios() {
+        TelaCadastrarFormularios = new JPanel();
+        TelaCadastrarFormularios.setLayout(new GridLayout(9, 2));
 
+        JLabel labelCategoria = new JLabel("Categoria:");
+        textFieldCategoria = new JTextField(20);
+        TelaCadastrarFormularios.add(labelCategoria);
+        TelaCadastrarFormularios.add(textFieldCategoria);
+
+
+        JLabel labelIdEmpresa = new JLabel("ID da Empresa:");
+        textFieldIdEmpresa = new JTextField(20);
+        TelaCadastrarFormularios.add(labelIdEmpresa);
+        TelaCadastrarFormularios.add(textFieldIdEmpresa);
+
+        JLabel labelIdFuncionario = new JLabel("ID do Funcionário:");
+        textFieldIdFuncionario = new JTextField(20);
+        TelaCadastrarFormularios.add(labelIdFuncionario);
+        TelaCadastrarFormularios.add(textFieldIdFuncionario);
+
+
+
+
+        CadastrarFormulariosButton = new JButton("Cadastrar Formulário");
+
+
+
+        setTitle("Cadastro de Formularios");
+        getContentPane().add(TelaCadastrarFormularios, BorderLayout.CENTER);
+        getContentPane().add(CadastrarFormulariosButton, BorderLayout.SOUTH);
+        pack();
+        setLocationRelativeTo(null);
+        CadastrarFormulariosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String conformidade = textFieldConformidade.getText();
+                String categoria = textFieldCategoria.getText();
+                String idEmpresa = textFieldIdEmpresa.getText();
+                int idFuncionario = Integer.parseInt(textFieldIdFuncionario.getText());
+
+                EmpresaCliente empresa = new EmpresaCliente();
+                Funcionario funcionario = new Funcionario();
+
+
+                Formulario novoFormulario = new Formulario(0, conformidade, categoria, empresa, funcionario);
+
+
+                FormularioDao formularioDao = DaoFactory.createFormularioDao();
+                formularioDao.insert(novoFormulario);
+
+                JOptionPane.showMessageDialog(CadastrarFormularios.this, "Formulário cadastrado com sucesso!");
+            }
+        });
+    }
+
+
+
+    public static void main(String[] args) {
+        CadastrarFormularios cadastro = new CadastrarFormularios();
+        cadastro.setVisible(true);
     }
 }
