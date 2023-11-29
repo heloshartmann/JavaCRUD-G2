4cb4269309dbf2f3a0a73ddaa472ce2b07984764
MestreDoPortal <joaovitorcand@outlook.com>
2023-11-29 13:22:48 -0300

    listar form

---
+++ src/application/ListarFormularios.java
@@ -0,0 +1,14 @@
+package application;
+
+
+import javax.swing.*;
+
+
+public class ListarFormularios extends JFrame {
+    private JPanel TelaListarFormularios;
+
+    public ListarFormularios () {
+        TelaListarFormularios = new JPanel();
+
+    }
+}
